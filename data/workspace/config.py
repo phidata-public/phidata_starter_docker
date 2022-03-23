9ea3d060849a3779144d40d9979d152e8e4c6145
Ashpreet Bedi <ashpreet@phidata.com>
2022-03-23 12:58:59 -0400

    update

--- data/workspace/config.py
+++ data/workspace/config.py
@@ -1,6 +1,5 @@
 from phidata.app.devbox import Devbox
 from phidata.app.postgres import PostgresDb
-from phidata.app.superset import Superset
 
 from phidata.workspace import WorkspaceConfig
 from phidata.infra.docker.config import DockerConfig
@@ -12,11 +11,11 @@ from phidata.infra.docker.config import DockerConfig
 ##  - Dev database: A postgres db running in a container for dev data
 ######################################################
 
-dev_pg_name = "dev_pg"
-dev_pg = PostgresDb(
-    name=dev_pg_name,
-    postgres_user="dev",
+pg_db_name = "pg_db"
+pg_db = PostgresDb(
+    name=pg_db_name,
     postgres_db="dev",
+    postgres_user="dev",
     postgres_password="dev",
     # You can connect to this db on port 5532 (on the host machine)
     container_host_port=5532,
@@ -24,12 +23,11 @@ dev_pg = PostgresDb(
 devbox = Devbox(
     # Init Airflow webserver when the container starts
     init_airflow_webserver=True,
-    db_connections={dev_pg_name: dev_pg.get_connection_url_docker()},
+    db_connections={pg_db_name: pg_db.get_connection_url_docker()},
     create_airflow_test_user=True,
 )
-superset = Superset(enabled=False)
 dev_docker_config = DockerConfig(
-    apps=[devbox, dev_pg, superset],
+    apps=[devbox, pg_db],
 )
 
 ## Configure the workspace
